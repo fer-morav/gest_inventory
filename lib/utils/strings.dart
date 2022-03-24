f3e71691ce15e2e6b3b2f26509f46f5a802bb833
fermorav <fer.1998.madrid@gmail.com>
2022-03-23 18:19:41 -0600

    Creacion de TextFieldMain, rediseño de la pantalla de registro de usuario

--- lib/utils/strings.dart
+++ lib/utils/strings.dart
@@ -1,8 +1,10 @@
 const app_name = "GestInventory";
 
-const title_login = "Registro de empleado";
+const title_register_user = "Registro de usuario";
+const title_login = "Iniciar Sesión";
 
 const button_login_admin = "Aceptar";
+const button_register_user = "Registrar";
 
-//const button_login_help = "Empleado";
-//const button_recover_password = "Recuperar Contraseña";
+const button_login_help = "Empleado";
+const button_recover_password = "Recuperar Contraseña";
