ea7c7dc02c9be953e4ea1334ec893c8c66d7135e
fermorav <fer.1998.madrid@gmail.com>
2022-05-12 01:30:19 -0500

    Creacion de apk

--- lib/components/StatisticsComponent.dart
+++ lib/components/StatisticsComponent.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../data/models/Sales.dart';
 import '../utils/colors.dart';
 
@@ -12,47 +11,49 @@ class StatisticsComponent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double getResponsiveText(double size) =>
-        size * sizeReference / MediaQuery.of(context).size.longestSide;
+        size * sizeReference / MediaQuery
+            .of(context)
+            .size
+            .longestSide;
 
-    return Container(
-      child: FloatingActionButton(
-        heroTag: null,
-        onPressed: () {},
-        backgroundColor: Colors.white,
-        elevation: 8,
-        isExtended: true,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              padding: const EdgeInsets.only(left: 10, right: 5),
-              child: Transform.scale(
-                scale: 1.5,
-                child: Icon(
-                  Icons.monetization_on_outlined,
-                  color: Colors.greenAccent,
+    return FloatingActionButton(
+      heroTag: null,
+      onPressed: () {},
+      backgroundColor: Colors.white,
+      elevation: 8,
+      isExtended: true,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        children: [
+          Container(
+            padding: const EdgeInsets.only(left: 10, right: 5),
+            child: Transform.scale(
+              scale: 1.5,
+              child: Icon(
+                Icons.monetization_on_outlined,
+                color: Colors.greenAccent,
+              ),
+              alignment: Alignment.center,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text(
+                sales.nombreProducto,
+                style: TextStyle(
+                  color: primaryColor,
+                  fontSize: getResponsiveText(15),
                 ),
-                alignment: Alignment.center,
+                textAlign: TextAlign.left,
               ),
             ),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.25,
-              child: Expanded(
-                child: Text(
-                  sales.nombreProducto,
-                  style: TextStyle(
-                    color: primaryColor,
-                    fontSize: getResponsiveText(15),
-                  ),
-                  textAlign: TextAlign.left,
-                ),
-              ),
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.25,
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.only(left: 30),
               child: Text(
                 "\$" + sales.total.toString(),
                 style: TextStyle(
@@ -62,9 +63,10 @@ class StatisticsComponent extends StatelessWidget {
                 textAlign: TextAlign.left,
               ),
             ),
-            Container(
-              padding: EdgeInsets.only(left: 20),
-              width: MediaQuery.of(context).size.width * 0.25,
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.only(left: 60),
               child: Text(
                 (sales.ventasMayoreo + sales.ventasUnitario).toString(),
                 style: TextStyle(
@@ -74,8 +76,8 @@ class StatisticsComponent extends StatelessWidget {
                 textAlign: TextAlign.left,
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
